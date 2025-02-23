432ee7b370d0b8cc03bae8bf5fbff4f3a53d7829
cvitaa11 <ivancvitkovic98@gmail.com>
2025-02-23 13:14:21 +0100

    feat: add s3 public access block

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -15,6 +15,15 @@ resource "aws_s3_bucket_versioning" "this" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   bucket = aws_s3_bucket.this.id
 
